37506f9c8592a7475e3a1efb60f32af9f6aafca8
mbrad26 <mbrad26@gmail.com>
2020-04-29 16:09:59 +0100

    entering players

--- Battle/app.rb
+++ Battle/app.rb
@@ -1,9 +1,19 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
-  
+
+  # get '/' do
+  #   'Testing infrastucture working!'
+  # end
+
   get '/' do
-    'Testing infrastucture working!'
+    erb :index
+  end
+
+  post '/names' do
+    @player_1_name = params[:player_1_name]
+    @player_2_name = params[:player_2_name]
+    erb :play
   end
 
   run! if app_file == $0
--- Battle/spec/testing_infrastructure_spec.rb
+++
@@ -1,6 +0,0 @@
-feature 'Testing infrastucture' do
-  scenario 'Can run app and check page content' do
-    visit('/')
-    expect(page).to have_content 'Testing infrastucture working!'
-  end
-end
